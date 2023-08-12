b8f214c6f553951b1f2d47248bfd49b32f1f5208
abdullah-rajab98 <abdullah.r.bakry@gmail.com>
2023-08-12 15:31:40 +0300

    ddd

--- src/dtos/role.dto.ts
+++ src/dtos/role.dto.ts
@@ -5,8 +5,9 @@ import { ApiProperty, PartialType } from "@nestjs/swagger";
 export class CreateRoleDto {
     @ApiProperty()
     name: string
+
+    @ApiProperty()
+    permission: [];
 }
-
-
 
 export class UpdateRoleDto extends PartialType(CreateRoleDto) { }
