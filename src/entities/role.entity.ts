ea3da5a8164062b788a99487112de0632d0a8ad9
abdullah-rajab98 <abdullah.r.bakry@gmail.com>
2023-08-27 16:09:47 +0300

    d

--- src/entities/role.entity.ts
+++ src/entities/role.entity.ts
@@ -7,12 +7,13 @@ import {
 import { OBaseEntity } from './OBaseEntity';
 import { User } from './user.entity';
 import { Permission } from './permission.entity';
-
+import { UserRole } from '../enums/userRole.enum';
 @Entity()
 export class Role extends OBaseEntity {
-    [x: string]: any;
-    @Column({})
-    name?: string;
+    @Column({
+        // type:()=>UserRole
+    })
+    name?: UserRole;
 
     @ApiPropertyOptional({ type: () => User })
     @OneToMany(() => User, u => u.role)
