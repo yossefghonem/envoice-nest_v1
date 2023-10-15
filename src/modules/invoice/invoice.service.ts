5c192d28231540d0bd29dc585fec4a11ee240a5a
bakr-rajab <abobakr.r.bakry@gmail.com>
2023-10-15 23:35:41 +0300

    test

--- src/modules/invoice/invoice.service.ts
+++ src/modules/invoice/invoice.service.ts
@@ -246,7 +246,7 @@ export class InvoiceService {
               taxType: tax.taxType.code,
               amount: +tax.amount,
               subType: tax.subTax.code,
-              rate: +tax.rate,
+              rate: +tax.rate * 10,
             };
           }),
         } as InvoicelineDto;
