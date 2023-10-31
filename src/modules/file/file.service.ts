6ce55145f4f2308786ea4b52b18185dfccfe5fc9
bakr-rajab <abobakr.r.bakry@gmail.com>
2023-10-31 15:39:43 +0200

    new

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -1,32 +1,14 @@
 
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from '../../entities/user.entity';
-import { readExcelFile } from '../../shared/excel-file-reader';
+// import { Injectable } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { Repository } from 'typeorm';
+// import { User } from '../../entities/user.entity';
+// // import { readExcelFile } from '../../shared/excel-file-reader';
 
-@Injectable()
-export class FileService {
-    constructor(
-        @InjectRepository(User)
-        private readonly userRepo: Repository<User>,
-    ) { }
-
-    async insertDataFromExcel(filePath: string) {
-        const dataFromExcel = await readExcelFile(filePath);
-
-        // Insert data into the database using the TypeORM repository
-        const entitiesToSave = dataFromExcel.map((item) => {
-            const user = new User();
-            // Map the properties from 'item' to the corresponding properties in 'entity'
-            user.name = item.name;
-            user.email = item.email;
-            user.password = item.password;
-            // this.userRepo.insert = item.user;
-            // Add other property mappings here
-            return user;
-        });
-
-        return await this.userRepo.save(entitiesToSave);
-    }
-}
+// @Injectable()
+// export class FileService {
+//   constructor(
+//     @InjectRepository(User)
+//     private readonly userRepo: Repository<User>,
+//   ) {}
+// }
