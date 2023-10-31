8b2eb75ecc4b8cf4c11d9ceb2e6dfaeb8f90808b
bakr-rajab <abobakr.r.bakry@gmail.com>
2023-10-31 21:52:24 +0200

    remove packAGWS

--- src/entities/units.entity.ts
+++ src/entities/units.entity.ts
@@ -1,37 +1,7 @@
-// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-// import { OBaseEntity } from './OBaseEntity';
-// import { ApiProperty } from '@nestjs/swagger';
-// import { UserRole } from '../enums/userRole.enum';
-// import * as bcrypt from 'bcrypt';
-// import { UserType } from '../enums/userType.enum';
-
 import { Column, Entity } from "typeorm";
 import { OBaseEntity } from "./OBaseEntity";
 import { ApiProperty } from "@nestjs/swagger";
 
-// export type UnitDocument = Unit & Document;
-
-// @Schema({
-//     autoIndex: true,
-//     timestamps: {
-//         createdAt: 'createdAt',
-//         updatedAt: 'updatedAt',
-//     },
-// })
-// export class Unit extends OBaseEntity {
-//     @ApiProperty()
-//     @Prop({})
-//     code: string;
-
-//     @ApiProperty()
-//     @Prop({})
-//     desc_en: string;
-
-//     @ApiProperty()
-//     @Prop({})
-//     desc_er: string;
-// }
-// export const UnitSchema = SchemaFactory.createForClass(Unit);
 
 @Entity()
 export class Units extends OBaseEntity {
